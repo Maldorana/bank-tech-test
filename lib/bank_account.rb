205900de0cbb8f2a35c8da9618e584c80cdff996
Sophie Bonneval <maldorana@gmail.com>
2021-10-25 20:56:08 +0100

    Refactor and add exceptions

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,16 +2,29 @@ class BankAccount
   attr_reader :balance
 
   def initialize
-    @balance = 0
+    @balance = 0.00
   end
 
   def deposit(amount)
+    raise 'Please make sure to enter a positive number' unless valid_number?(amount)
+
     @balance += amount
   end
 
   def withdraw(amount)
-    raise 'You cannot withdraw more than your current balance' if @balance < amount
-    
+    raise 'Please make sure to enter a positive number' unless valid_number?(amount)
+    raise 'You cannot withdraw more than your current balance' unless insufficient_funds?(amount)
+
     @balance -= amount
+  end
+
+  private
+
+  def insufficient_funds?(amount)
+    @balance > amount
+  end
+
+  def valid_number?(amount)
+    amount.is_a?(Numeric) && amount >= 0.01
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,25 +3,38 @@ require 'bank_account'
 describe BankAccount do
   describe 'basic banking functions' do
     it 'has an initial balance of 0' do
-      expect(subject.balance).to eq(0)
+      expect(subject.balance).to eq(0.00)
     end
 
     it 'can deposit to the balance' do
       subject.deposit(100)
-      expect(subject.balance).to eq(100)
+      expect(subject.balance).to eq(100.00)
     end
 
     it 'can withdraw from the balance' do
       subject.deposit(100)
-      expect(subject.balance).to eq(100)
+      expect(subject.balance).to eq(100.00)
       subject.withdraw(50)
-      expect(subject.balance).to eq(50)
+      expect(subject.balance).to eq(50.00)
     end
   end
 
   describe 'exceptions and errors' do
     it 'raises an error if withdrawal amount is higher than balance' do
       expect { subject.withdraw(1) }.to raise_error('You cannot withdraw more than your current balance')
+    end
+
+    it 'raises an error if deposit amount given isnt a positive number' do
+      expect { subject.deposit(-1) }.to raise_error('Please make sure to enter a positive number')
+      expect { subject.deposit([1, 3]) }.to raise_error('Please make sure to enter a positive number')
+      expect { subject.deposit("Hello") }.to raise_error('Please make sure to enter a positive number')
+    end
+
+    it 'raises an error if withdrawal amount given isnt a positive number' do
+      subject.deposit(100)
+      expect { subject.withdraw(-1) }.to raise_error('Please make sure to enter a positive number')
+      expect { subject.withdraw([1, 3]) }.to raise_error('Please make sure to enter a positive number')
+      expect { subject.withdraw("Hello") }.to raise_error('Please make sure to enter a positive number')
     end
   end
 end
